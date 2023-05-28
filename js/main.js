4a1df3e2f50419ed3674e0feb9e8da0f00e2600b
birkelbot <mbirkel91@gmail.com>
2023-05-28 10:36:27 -0500

    First working version of an actual website

--- js/main.js
+++ js/main.js
@@ -1,19 +1,61 @@
-function set100vhVar() {
-  // If less than most tablets, set CSS var to window height.
-  let value = "100vh"
+(function($) {
 
-  // If window size is iPad or smaller, then use JS to set screen height.
-  if (window.innerWidth && window.innerWidth <= 1024) {
-    value = `${window.innerHeight}px`
+  var breakpoints = new Map();
+  breakpoints.set('sm', 576);
+  breakpoints.set('md', 768);
+  breakpoints.set('lg', 992);
+  breakpoints.set('xl', 1200);
+  breakpoints.set('xxl', 1400);
+
+  function setPageLayout() {
+    if ($(window).width() < breakpoints.get('sm')) {
+      // One column.
+      $('.names').css('height', 'auto');
+      $('.chunk').css('width', '100%');
+    } else if ($(window).width() < breakpoints.get('md')) {
+      // Two columns.
+      $('.names').css('height', $(window).width()*2.4);
+      $('.chunk').css('width', '50%');
+    } else if ($(window).width() < breakpoints.get('xl')) {
+      // Three columns
+      $('.names').css('height', $(window).width()*1.05);
+      $('.chunk').css('width', '33%');
+    } else {
+      // Four columns.
+      $('.names').css('height', $(window).width()*0.627);
+      $('.chunk').css('width', '25%');
+    }
+
+    $('.scroll-button').css('left', $(window).width()/2 - $('.scroll-button').width()/2);
   }
-  document.documentElement.style.setProperty("--real100vh", value)
-}
 
+  function removeScrollButton() {
+    $('.scroll-button').fadeOut(300, function() { $('.scroll-button').remove(); })
+  }
 
-window.onload = (event) => {
-  set100vhVar();
-};
+  $(function() {
 
-window.onresize = function() {
-  set100vhVar();
-}
+    setPageLayout();
+
+    $(window).resize(function() {
+      setPageLayout();
+    });
+
+    $('.scroll-button').click(function() {
+      removeScrollButton()
+    });
+
+    $(window).scroll(function () {
+      // Remove the scroll button if the names section is now at least 1/4 of the viewport.
+      if ($('.names').offset().top < ($(window).scrollTop() + ($(window).height() * 3 / 4))) {
+        removeScrollButton();
+      }
+      // Remove the scroll button if the user scrolls to the bottom of the page.
+      if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        removeScrollButton();
+      }
+    });
+
+  });
+
+})(jQuery);
